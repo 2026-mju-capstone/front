5cf908d631e2f5500ff44b9e2867a5158add02dc
Dongrang072 <52373572+Dongrang072@users.noreply.github.com>
2026-05-08 17:48:35 +0900

    Fix: profile 조회로 토큰 유효성을 사전에 검증하는 구조

--- hooks/queries/useUserQueries.ts
+++ hooks/queries/useUserQueries.ts
@@ -1,9 +1,11 @@
-import {useQuery} from '@tanstack/react-query';
+import {useQuery, UseQueryOptions} from '@tanstack/react-query';
 import {userService} from '@/api/services/user';
+import {UserProfile} from '@/api/types';
 
-export const useProfile = () => {
+export const useProfile = (options?: Partial<UseQueryOptions<UserProfile>>) => {
     return useQuery({
         queryKey: ['userProfile'],
         queryFn: () => userService.getProfile(),
+        ...options,
     });
 };
