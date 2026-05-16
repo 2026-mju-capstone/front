421afa271a3a5342ad07748dbdf7699bfeebe388
lalaalal <bontin0128@gmail.com>
2026-05-16 21:53:15 +0900

    Feat: 알림 종류별 처리
    - notifee를 사용해 알림 띄우기
    - expo-build-properties, @notifee/react-native 추가
    
    Resolves: #26

--- hooks/use-notifications.ts
+++ hooks/use-notifications.ts
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { PermissionsAndroid, Platform } from "react-native";
 import { Href, router } from 'expo-router';
 import { ROUTES } from "@/constants/url";
+import notifee, { EventType, Event, EventDetail, AndroidAction, AndroidImportance } from '@notifee/react-native';
+import { chatService } from "@/api/services/chat";
 
 export interface NotificationData {
     type?: string;
@@ -18,11 +20,64 @@ export type TokenCallback = (token: string) => void;
 
 export type OpenNotificationAction = (remoteMessage: ZoopickRemoteMessage) => void;
 
+
+const createNotification = async (message: ZoopickRemoteMessage, actions: AndroidAction[] = []) => {
+    const data = message?.data;
+    const title = data?.title;
+    const body = data?.body;
+    const channelId = await notifee.createChannel({
+        id: 'zoopick_default',
+        name: 'Default Zooipck Channel',
+        importance: AndroidImportance.HIGH,
+    });
+
+    await notifee.displayNotification({
+        title,
+        body,
+        data: data as Notification['data'],
+        android: {
+            channelId,
+            actions,
+            pressAction: {
+                id: 'openPage',
+                launchActivity: 'default'
+            }
+        }
+    })
+}
+
+const createSimpleNotification = async (message: ZoopickRemoteMessage) => {
+    createNotification(message);
+}
+
+const createChatNotification = async (message: ZoopickRemoteMessage) => {
+    createNotification(message, [
+        {
+            title: '읽음',
+            pressAction: { id: 'read' }
+        },
+        {
+            title: '답장',
+            pressAction: { id: 'reply' },
+            input: true
+        }
+    ]);
+}
+
+
+const NOTIFICATION_DISPLAY_REGISTRY: Record<string, (message: ZoopickRemoteMessage) => void> = {
+    CHAT_MESSAGE: createChatNotification,
+    ITEM_RETURNED: createSimpleNotification,
+    MATCH_FOUND: createSimpleNotification,
+    THEFT_SUSPECTED: createSimpleNotification,
+    LOCKER_READY: createSimpleNotification
+}
+
 /**
  * FCM에서 data의 모든 value는 string으로 변환되어 들어옵니다.
  * 숫자나 다른 타입을 사용한다면 파싱이 필요합니다.
  */
-const ROUTE_MAP: Record<string, (data: any) => Href> = {
+const ROUTING_REGISTRY: Record<string, (data: NotificationData) => Href> = {
     CHAT_MESSAGE: (data) => ({
         pathname: ROUTES.CHAT_ROOM,
         params: { roomId: Number(data.room_id) }
@@ -31,17 +86,17 @@ const ROUTE_MAP: Record<string, (data: any) => Href> = {
         pathname: ROUTES.LOST_ITEM_DETAIL,
         params: { id: Number(data.item_post_id) }
     }),
-    MATCH_FOUND: (data) => ({
+    MATCH_FOUND: () => ({
         pathname: ROUTES.MATCHES,
-        params: { }
+        params: {}
     }),
-    THEFT_SUSPECTED: (data) => ({
+    THEFT_SUSPECTED: () => ({
         pathname: ROUTES.CCTV_RESULT,
-        paramas: { }
+        paramas: {}
     }),
-    LOCKER_READY: (data) => ({
+    LOCKER_READY: () => ({
         pathname: ROUTES.MYPAGE,
-        params: { }
+        params: {}
     })
 }
 
@@ -88,60 +143,86 @@ const requestNotificationPermission = async () => {
     return false;
 };
 
-export function useNotifications(
-    openNotificationAction: OpenNotificationAction | null = handleNotificationRouting,
-) {
+export const displayNotification = async (message: ZoopickRemoteMessage) => {
+    const data: NotificationData | undefined = message?.data;
+    if (!data) return;
+    const type: string | undefined = data?.type;
+    if (!type) return;
+    const displayer = NOTIFICATION_DISPLAY_REGISTRY[type];
+    displayer(message);
+}
+
+export function useNotifications() {
+    messaging().setBackgroundMessageHandler(displayNotification);
     useEffect(() => {
+        messaging().registerDeviceForRemoteMessages();
+
         if (!requestNotificationPermission())
             console.log("Notification permission denied");
 
-        const unsubscribeTokenRefresh =
-            messaging().onTokenRefresh(sendTokenToServer);
-
-        // 포그라운드 메시지 핸들러
-        const unsubscribeMessage = messaging().onMessage(
-            (remoteMessage: ZoopickRemoteMessage) => {
-            },
-        );
-
-        // 알림 클릭 핸들러 (백그라운드에서 열렸을 때)
-        const unsubscribeNotificationOpened = messaging().onNotificationOpenedApp(
-            (remoteMessage: ZoopickRemoteMessage) => {
-                if (openNotificationAction) openNotificationAction(remoteMessage);
-            },
-        );
-
-        // 앱이 완전히 종료된 상태에서 알림을 클릭해 열렸을 때
-        messaging()
-            .getInitialNotification()
-            .then((remoteMessage: ZoopickRemoteMessage | null) => {
-                if (remoteMessage && openNotificationAction)
-                    openNotificationAction(remoteMessage);
-            });
+        messaging().onMessage(displayNotification);
+        const unsubscribeTokenRefresh = messaging().onTokenRefresh(sendTokenToServer);
+        const unsubscribeForegroundEvent = notifee.onForegroundEvent(notificationEventHandler);
+        notifee.onBackgroundEvent(notificationEventHandler);
 
         return () => {
             unsubscribeTokenRefresh();
-            unsubscribeMessage();
-            unsubscribeNotificationOpened();
+            unsubscribeForegroundEvent();
         };
     }, []);
 }
 
-
-
-export const handleNotificationRouting = (remoteMessage: ZoopickRemoteMessage) => {
-    const type = remoteMessage?.data?.type;
+export const handleNotificationRouting = (data: NotificationData) => {
+    const type = data.type;
     if (!type) {
         console.warn("This notification does not have type");
         return;
     }
 
-    const getHref = ROUTE_MAP[type];
+    const getHref = ROUTING_REGISTRY[type];
     if (!getHref) {
         console.warn(`Unsupported notification type : ${type}`);
         return;
     }
 
-    const href = getHref(remoteMessage.data);
+    const href = getHref(data);
     router.navigate(href);
 }
+
+const readChatMessages = (data: NotificationData) => {
+    const roomId: number = Number(data?.room_id);
+    // Not implemented
+}
+
+const sendChatMessage = (data: NotificationData, input: string | undefined) => {
+    const roomId: number = Number(data?.room_id);
+    if (input)
+        chatService.sendMessage(roomId, input);
+}
+
+const PRESS_ACTION_REGISTRY: Record<string, (data: NotificationData, input: string | undefined) => void> = {
+    openPage: handleNotificationRouting,
+    read: readChatMessages,
+    reply: sendChatMessage
+};
+
+async function notificationEventHandler(observer: Event) {
+    const type: EventType = observer.type;
+    const detail: EventDetail = observer.detail;
+
+    const { notification, pressAction, input } = detail;
+    if (!notification)
+        return;
+
+    const data: NotificationData = notification.data as NotificationData;
+    switch (type) {
+        case EventType.ACTION_PRESS:
+        case EventType.PRESS:
+            if (!pressAction) break;
+            const action = PRESS_ACTION_REGISTRY[pressAction.id];
+            action(data, input);
+            break;
+    }
+}
+
+
